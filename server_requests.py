dd2d9502f188f7b06040f224fecfa572d2b9fd94
Liam Kirsh <liam.kirsh@riseup.net>
2015-10-10 22:36:07 -0700

    omers changes

--- server_requests.py
+++ server_requests.py
@@ -23,23 +23,5 @@ def init_download_on_server(JSON):
 def poll_server(QUERY):
     pass #TODO
 
-init_download_on_server('{"album": "Good Girl Gone Bad: Reloaded", "thumb": "https://i.scdn.co/image/b1244db3be7cb3c1fd05555c2e53dc5d2b94176d", "title": "Umbrella", "artist": "Rihanna"}')
-
-"""
-#get query from user
-QUERY = argv[1]
-#get metadata from server
-metadata = get_metadata_from_server(QUERY)
-#ask user if this is acceptable
-OK = argv[2]
-if OK:
-    init_download_on_server(QUERY)
-else:
-    quit()
-here_yet = False
-while not here_yet:
-    here_yet, audio = poll_server(QUERY)
-    time.sleep(1)
-#enjoy the music
-play(audio)
-"""
+if __name__ == '__main__':
+    init_download_on_server('{"album": "Good Girl Gone Bad: Reloaded", "thumb": "https://i.scdn.co/image/b1244db3be7cb3c1fd05555c2e53dc5d2b94176d", "title": "Umbrella", "artist": "Rihanna"}')
