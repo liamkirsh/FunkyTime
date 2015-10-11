751d4e4ff65f792fba5c547b52c6a45413e9a20e
Liam Kirsh <liam.kirsh@riseup.net>
2015-10-11 05:36:13 -0700

    actually adding in error handling for spotify api lookup

--- Spotify.py
+++ Spotify.py
@@ -6,6 +6,10 @@ import json
 script, query = sys.argv
 r = requests.get('https://api.spotify.com/v1/search?q=' + quote(query) + '&type=track')
 spot = r.json()
+if spot["tracks"]["total"] == 0:
+    print
+    sys.stdout.flush()
+    sys.exit(0)
 result = {"album": spot["tracks"]["items"][0]["album"]["name"], "artist": spot["tracks"]["items"][0]["artists"][0]["name"], "name": spot["tracks"]["items"][0]["name"], "thumb": spot["tracks"]["items"][0]["album"]["images"][1]["url"]}
 
 print json.dumps(result)
