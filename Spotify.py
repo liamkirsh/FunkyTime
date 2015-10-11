7a0b353fd3c1139345bdb5b3d091787c49552e85
Liam Kirsh <liam.kirsh@riseup.net>
2015-10-10 19:06:05 -0700

    Updated scripts from server

--- Spotify.py
+++ Spotify.py
@@ -1,13 +1,13 @@
 import requests
 import sys
 from urllib import quote
+import json
 
 script, query = sys.argv
-#print 'https://api.spotify.com/v1/search?q=' + quote(query)
-#sys.exit(0)
 r = requests.get('https://api.spotify.com/v1/search?q=' + quote(query) + '&type=track')
-json = r.json()
+spot = r.json()
+result = {"album": spot["tracks"]["items"][0]["album"]["name"], "artist": spot["tracks"]["items"][0]["artists"][0]["name"], "name": spot["tracks"]["items"][0]["name"], "thumb": spot["tracks"]["items"][0]["album"]["images"][1]["url"]}
 
-print json["tracks"]["items"][0]["album"]["name"]
+print json.dumps(result)
 sys.stdout.flush()
 sys.exit(0)
--- first_result.py
+++ first_result.py
@@ -2,10 +2,6 @@ import sys
 from KickassAPI import Search, Latest, User, CATEGORY, ORDER
 
 for t in Search(sys.argv[1], category=CATEGORY.MUSIC, order=ORDER.SEED):
-    #print "TITLE:"
-    #print t.name
-    #print "LINK"
     print 'http://' + t.download_link[2:]
-    sys.stdout.flush()
-    sys.exit(0)
+    sys.exit(0) 
 
